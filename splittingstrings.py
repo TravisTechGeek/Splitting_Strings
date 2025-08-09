43b41a5d2dd72a2801c1ff41e403450a56a15de5
Travis Paul Pyle <travispyle@gmail.com>
2025-08-09 09:45:51 -0400

    Add files via upload

---
+++ splittingstrings.py
@@ -0,0 +1,6 @@
+line_one = "The sky has given over"
+
+# Use .split() to create a list called line_one_words that contains each word in this line of poetry.
+line_one_words = line_one.split()
+
+print(line_one_words)
